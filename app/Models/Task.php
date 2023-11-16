9b4de9aa08dea5faa0288c62c7c693f610934289
diegodevweb <diego.devwebb@gmail.com>
2023-11-16 19:58:12 -0300

    fix: Logo agora aparece na guia de tarefas

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,8 @@
 
 declare(strict_types=1);
 
-namespace App\Models\Admin;
+namespace App\Models;
 
-use App\Models\TaskGroup;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
