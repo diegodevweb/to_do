a32390b817412d5caf3ed46efac2b160200c2061
diegodevweb <diego.devwebb@gmail.com>
2023-11-15 22:36:08 -0300

    feat: Centralizando a logo e o nome do app

--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -1,6 +1,6 @@
-<div class="flex justify-start">
-    <div>
-        <img src="../devtek.png" alt="" class="w-10 h-10">
+<div class="flex justify-start items-center">
+    <div class="flex justify-start">
+        <a href=" http://localhost/admin"><img src="../devtek.png" alt="" class="w-10 h-10"></a>
     </div>
     <div class="pt-1 pl-2 text-2xl font-bold tracking-tight filament-brand dark:text-white">
         <a href="http://localhost/admin">{{ config('app.name') }}</a>
